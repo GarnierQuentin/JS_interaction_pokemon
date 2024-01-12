5303acd8ef7245dc00cfadf8235b0f6350886b18
Quentin Garnier <113943161+GarnierQuentin@users.noreply.github.com>
2024-01-12 11:58:43 +0100

    🎨 spliting js files

---
+++ prduit.js
@@ -0,0 +1,49 @@
+
+let apiUrl3 = `https://pokeapi.co/api/v2/pokemon?limit=30&offset=0`;
+const pokemons = document.querySelector('.pokemons');
+  
+fetch(apiUrl3)
+    .then(response => {
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.json();
+    })
+    
+    .then(data => {
+    data.results.forEach(element => {
+        fetch(`https://pokeapi.co/api/v2/pokemon/${element.name}`)
+        .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+        })
+        
+        .then(data => {
+
+            const pokemon = document.createElement('div');
+            pokemon.innerHTML = `<div class="pokemon">
+            <h1>${data.name.charAt(0).toUpperCase() + data.name.slice(1)}</h1>
+            <p>Type(s) :</p>
+            <ul>
+            ${data.types.map(type => `<li>${type.type.name}</li>`).join('')}
+            </ul>
+            <img src="${data.sprites.front_default}" alt="pokeball">
+            ${data.stats.map(element => `<p>${element.stat.name} : ${element.base_stat}</p>`).join('')}
+            <button>Ajouter au panier</button>
+            </div>`;
+
+            pokemons.appendChild(pokemon);
+        })
+
+        .catch(error => {
+        console.error(error);
+        });
+    });
+    })
+
+    .catch(error => {
+    console.error(error);
+    });
+
