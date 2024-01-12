136513af9bbfc59e866823399aa8b75fdd540c98
Quentin Garnier <113943161+GarnierQuentin@users.noreply.github.com>
2024-01-12 09:29:58 +0100

    👽️ fetching API

--- script.js
+++ script.js
@@ -9,25 +9,21 @@ selected_image.forEach((element) => {
     });
 });
 
-// let apiUrl3 = `https://pokeapi.co/api/v2/pokemon/1`;
+let apiUrl3 = `https://pokeapi.co/api/v2/pokemon/1`;
   
-// fetch(apiUrl3)
-//     .then(response => {
-//     if (!response.ok) {
-//         throw new Error(`HTTP error! Status: ${response.status}`);
-//     }
-//     return response.json();
-//     })
-//     .then(data => {
-//     console.log(data)
-//     selected_image.src = data.sprites.front_default
-//     nom.innerHTML = data.name
-//     error_text.innerHTML = ""
-//     })
-//     .catch(error => {
-//     console.error("AAAAAAAAH ... ce pokemon n'existe pas !");
-//     selected_image.src = ""
-//     nom.innerHTML = ""
-//     error_text.innerHTML = "AAAAAAAAH ... ce pokemon n'existe pas !"
-//     });
+fetch(apiUrl3)
+    .then(response => {
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.json();
+    })
+    
+    .then(data => {
+    console.log(data)
+    })
+
+    .catch(error => {
+    console.error("AAAAAAAAH ... ce pokemon n'existe pas !");
+    });
 
