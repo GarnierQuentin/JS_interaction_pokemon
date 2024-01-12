a695906579865cdeac432084d118d314bf6bf716
Quentin Garnier <113943161+GarnierQuentin@users.noreply.github.com>
2024-01-12 09:26:56 +0100

    ⚡️ navbar animation

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
-let apiUrl3 = `https://pokeapi.co/api/v2/pokemon/1`;
-  
-fetch(apiUrl3)
-    .then(response => {
-    if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    return response.json();
-    })
-    .then(data => {
-    console.log(data)
-    selected_image.src = data.sprites.front_default
-    nom.innerHTML = data.name
-    error_text.innerHTML = ""
-    })
-    .catch(error => {
-    console.error("AAAAAAAAH ... ce pokemon n'existe pas !");
-    selected_image.src = ""
-    nom.innerHTML = ""
-    error_text.innerHTML = "AAAAAAAAH ... ce pokemon n'existe pas !"
+const selected_image = document.querySelectorAll(".nav_link");   
+
+selected_image.forEach((element) => {
+    element.addEventListener("mouseover", () => {
+        element.classList.add("bounce");
+        setTimeout(() => {
+            element.classList.remove("bounce");
+        }, 500);
     });
+});
+
+// let apiUrl3 = `https://pokeapi.co/api/v2/pokemon/1`;
+  
+// fetch(apiUrl3)
+//     .then(response => {
+//     if (!response.ok) {
+//         throw new Error(`HTTP error! Status: ${response.status}`);
+//     }
+//     return response.json();
+//     })
+//     .then(data => {
+//     console.log(data)
+//     selected_image.src = data.sprites.front_default
+//     nom.innerHTML = data.name
+//     error_text.innerHTML = ""
+//     })
+//     .catch(error => {
+//     console.error("AAAAAAAAH ... ce pokemon n'existe pas !");
+//     selected_image.src = ""
+//     nom.innerHTML = ""
+//     error_text.innerHTML = "AAAAAAAAH ... ce pokemon n'existe pas !"
+//     });
+
