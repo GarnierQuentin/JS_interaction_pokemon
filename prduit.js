05170b2681db5140a9d3007407f1f9dd11cc73f6
Quentin Garnier <113943161+GarnierQuentin@users.noreply.github.com>
2024-01-13 01:14:31 +0100

    ⚡️auto refresh after reservation

--- prduit.js
+++ prduit.js
@@ -23,34 +23,38 @@ fetch(apiUrl3)
         
         .then(data => {
 
-            const pokemon = document.createElement('div');
-            pokemon.innerHTML = `<div class="pokemon">
-            <h1>${data.name.charAt(0).toUpperCase() + data.name.slice(1)}</h1>
-            <p>Type(s) :</p>
-            <ul>
-            ${data.types.map(type => `<li>${type.type.name}</li>`).join('')}
-            </ul>
-            <img src="${data.sprites.front_default}" alt="pokeball">
-            ${data.stats.map(element => `<p>${element.stat.name} : ${element.base_stat}</p>`).join('')}
-            </div>`;
-
-            const reservationButton = document.createElement('button');
-            reservationButton.classList.add('reservation_button');
-            reservationButton.textContent = 'Ajouter au panier';
-
-            pokemon.querySelector('.pokemon').appendChild(reservationButton);
-            pokemons.appendChild(pokemon);
-
-
             if (!localStorage.getItem("reservation_list")) {
                 localStorage.setItem("reservation_list", JSON.stringify([]));
             }
-            
-            reservationButton.addEventListener("click", () => {
-                let reservation_list = JSON.parse(localStorage.getItem("reservation_list"));
-                reservation_list.push(data.name);
-                localStorage.setItem("reservation_list", JSON.stringify(reservation_list));
-            });
+
+            if (!localStorage.getItem("reservation_list").includes(data.name)) {
+                const pokemon = document.createElement('div');
+                pokemon.innerHTML = `<div class="pokemon">
+                <h1>${data.name.charAt(0).toUpperCase() + data.name.slice(1)}</h1>
+                <p>Type(s) :</p>
+                <ul>
+                ${data.types.map(type => `<li>${type.type.name}</li>`).join('')}
+                </ul>
+                <img src="${data.sprites.front_default}" alt="pokeball">
+                ${data.stats.map(element => `<p>${element.stat.name} : ${element.base_stat}</p>`).join('')}
+                </div>`;
+
+                const reservationButton = document.createElement('button');
+                reservationButton.classList.add('reservation_button');
+                reservationButton.textContent = 'Ajouter au panier';
+                reservationButton.href = '';
+
+                pokemon.querySelector('.pokemon').appendChild(reservationButton);
+                pokemons.appendChild(pokemon);
+
+                reservationButton.addEventListener("click", () => {
+                    let reservation_list = JSON.parse(localStorage.getItem("reservation_list"));
+                    reservation_list.push(data.name);
+                    localStorage.setItem("reservation_list", JSON.stringify(reservation_list));
+
+                    location.reload();
+                });
+            }
         })
 
         .catch(error => {
