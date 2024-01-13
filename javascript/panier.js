a7e4aa15592e894992a1c43fb7ac0f4c569ea3d1
Quentin Garnier <113943161+GarnierQuentin@users.noreply.github.com>
2024-01-13 19:36:32 +0100

    ✨ shopping list and total price feature

--- javascript/panier.js
+++ javascript/panier.js
@@ -1,5 +1,20 @@
 const panier_items = document.querySelector('.panier_items');
 
+
+
+if (!localStorage.getItem("current_money")) {
+    localStorage.setItem("current_money", 250);
+}
+
+let current_money = document.querySelector('.solde');
+current_money.innerHTML = `Solde : ${localStorage.getItem("current_money")}€`;
+
+
+
+const list = document.querySelector('.list');
+const total = document.querySelector('.total');
+let total_price = 0;
+
 if (localStorage.getItem("reservation_list")) {
     let listeReservation = JSON.parse(localStorage.getItem("reservation_list"));
     console.log(listeReservation);
@@ -35,13 +50,27 @@ if (localStorage.getItem("reservation_list")) {
             <div class="pokemon_decision">
             <h2>Prix : ${prix}€</h2>
             </div>`;
+
+            total_price += prix;
+
             const pokemonDecision = pokemonInfo.querySelector(".pokemon_decision");
             pokemonDecision.appendChild(checker);
 
             panier_items.appendChild(pokemonInfo);
+
+
+            const pokemon_shopping = document.createElement('div');
+            pokemon_shopping.classList.add('pokemon_shopping');
+
+            pokemon_shopping.innerHTML = `<p> <b>${data.name.charAt(0).toUpperCase() + data.name.slice(1)}</b> - ${prix}€</p>`;
+
+            list.appendChild(pokemon_shopping);
+
+            total.innerHTML = `Total : ${total_price}€`;
         })
 
 
         .catch(error => console.error('Error:', error));
         });
 }
+
